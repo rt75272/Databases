b6f2ab156c53d8463f28591848393662c0c92b64
rt75272 <rt75272@gmail.com>
2023-09-26 18:06:51 -0500

    Fixed variable link between mysql tables.
        - Just need to fill the database and then debug.

--- user_account.sql
+++ user_account.sql
@@ -4,12 +4,13 @@ use store;
 create table if not exists accounts (
     acc_id          varchar(15) not null,
     acc_num         numeric(15),
-    user_acc_id     varchar(15),
+    user_name         varchar(15),
     primary key (acc_id)
 );
 
 create table if not exists users (
     user_id         varchar(15) not null,
+    acc_id          varchar(15),
     user_name       varchar(15),
     phone_number    char(10),
     email           varchar(50),
@@ -19,16 +20,17 @@ create table if not exists users (
 
 create table if not exists payments (
     pay_id          varchar(15) not null,
-    pay_user_id     varchar(15),
+    user_id         varchar(15),
     pay_desc        varchar(30),
     pay_amount      numeric(10),
     pay_date        varchar(15),
     primary key (pay_id)
 );
 
-create table if not exists (
+create table if not exists user_login (
     login_id        varchar(15) not null,
-    login_user_name varchar(20),
+    user_id         varchar(15),
+    user_name       varchar(20),
     user_password   varchar(15),
     primary key (login_id)
 );
